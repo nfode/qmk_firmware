deeb3d2b1840bf92be0fd51247779a4211a193ef
Dimitris Mantzouranis <d3xter93@gmail.com>
2021-11-22 00:49:44 +0200

    Disable USB driver restart after remote wakeup

--- tmk_core/protocol/chibios/chibios.c
+++ tmk_core/protocol/chibios/chibios.c
@@ -223,7 +223,9 @@ void protocol_task(void) {
             /* Remote wakeup */
             if (suspend_wakeup_condition()) {
                 usbWakeupHost(&USB_DRIVER);
+#if !defined(SN32F24xx)
                 restart_usb_driver(&USB_DRIVER);
+#endif
             }
         }
         /* Woken up */
